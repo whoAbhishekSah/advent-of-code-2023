cce0a70160862446e3aed35dde10e8eb5ac307bc
Abhishek Sah <abhi.sah.97@gmail.com>
2023-12-01 23:13:13 +0530

    day 1

---
+++ day1/main.py
@@ -0,0 +1,97 @@
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
+word_to_digit_map = {
+    "one" : 1,
+    "two" : 2,
+    "three" : 3,
+    "four" : 4,
+    "five" : 5,
+    "six" : 6,
+    "seven" : 7,
+    "eight" : 8,
+    "nine" : 9
+}
+
+def get_first_digit_from_line(line):
+    res = ""
+    for x in range(len(line)):
+        if line[x].isdigit():
+            res = res + line[x]
+            break
+        else:
+            sliced_line = line[x:]
+            if sliced_line.startswith("one"):
+                res = res + "1"
+                break
+            elif sliced_line.startswith("two"):
+                res = res + "2"
+                break
+            elif sliced_line.startswith("three"):
+                res = res + "3"
+                break
+            elif sliced_line.startswith("four"):
+                res = res + "4"
+                break
+            elif sliced_line.startswith("five"):
+                res = res + "5"
+                break
+            elif sliced_line.startswith("six"):
+                res = res + "6"
+                break
+            elif sliced_line.startswith("seven"):
+                res = res + "7"
+                break
+            elif sliced_line.startswith("eight"):
+                res = res + "8"
+                break
+            elif sliced_line.startswith("nine"):
+                res = res + "9"
+                break
+    return res
+
+def get_last_digit_from_line(line):
+    res = ""
+    for x in range(1, len(line)+1, 1):
+        # print("val of x", x)
+        if line[-1*x].isdigit():
+            res= line[-1*x]
+            break
+        else:
+            sliced_line = line[-1*x:]
+            if sliced_line.startswith("one"):
+                res = "1"
+                break
+            elif sliced_line.startswith("two"):
+                res = "2"
+                break
+            elif sliced_line.startswith("three"):
+                res = "3"
+                break
+            elif sliced_line.startswith("four"):
+                res = "4"
+                break
+            elif sliced_line.startswith("five"):
+                res = "5"
+                break
+            elif sliced_line.startswith("six"):
+                res = "6"
+                break
+            elif sliced_line.startswith("seven"):
+                res = "7"
+                break
+            elif sliced_line.startswith("eight"):
+                res = "8"
+                break
+            elif sliced_line.startswith("nine"):
+                res = "9"
+                break
+    return res
+
+sum = 0
+for line in lines:
+    first_digit_str = get_first_digit_from_line(line)
+    last_digit_str = get_last_digit_from_line(line)
+    sum = sum + int(""+first_digit_str+last_digit_str)
+
+print(sum)
