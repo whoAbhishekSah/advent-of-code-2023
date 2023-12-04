7b702329e105f944f9baedcbbc8c49ef370d04aa
Abhishek Sah <abhi.sah.97@gmail.com>
2023-12-04 22:11:08 +0530

    day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 file1 = open(os.path.dirname(os.path.abspath(__file__)) + '/input.txt', 'r')
 lines = file1.readlines()
@@ -13,6 +14,11 @@ for line in lines:
         row.append(char)
     input_matrix.append(row)
 
+class Gear():
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
 class NumberInMatrix():
     def __init__(self, val, i1, j1, i2, j2) -> None:
         self.start_x = i1
@@ -24,6 +30,17 @@ class NumberInMatrix():
 #find all numbers and store the coordinates of start and end number
 # now search in the surrounding rectangle for any symbol which is not a dot(.)
 all_numbers = []
+all_gears = []
+
+i = 0
+while( i < len(input_matrix)):
+    j = 0
+    while (j < len(input_matrix[i])):
+        if (input_matrix[i][j] == '*'):
+            all_gears.append(Gear(i, j))
+        j = j + 1
+    i = i + 1
+
 i = 0
 while( i < len(input_matrix)):
     j = 0
@@ -40,7 +57,7 @@ while( i < len(input_matrix)):
             i2 = i
             j2 = j
             # print("came here with val, i2, j2", current_number, i2, j2)
-            print("found", current_number, "b/w cordinates (", i1, ",", j1, ") and (", i2, ",", j2 -1, ")")
+            # print("found", current_number, "b/w cordinates (", i1, ",", j1, ") and (", i2, ",", j2 -1, ")")
             found_number = NumberInMatrix(int(current_number), i1, j1, i2, j2-1) 
             all_numbers.append(found_number)
             if (j >= len(input_matrix[i])):
@@ -53,51 +70,41 @@ sum = 0
 def is_valid_position(i, j):
     return i>=0 and i<len(input_matrix) and j>=0 and j<len(input_matrix[i])
 
-def is_symbol(i, j):
-    return (not input_matrix[i][j].isalnum()) and input_matrix[i][j]!='.'
-        
+def is_number_adjacent_to(number, x, y):
+    if (x== number.start_x):
+        return y>=number.start_y and y<=number.end_y
+    
+    return False
 
-for number in all_numbers:
-    i1 = number.start_x
-    j1 = number.start_y
-    i2 = number.end_x
-    j2 = number.end_y
+def get_adjacency_of_gear(gear):
+    res = []
+    res.append({'x': gear.x-1, 'y': gear.y-1})
+    res.append({'x':gear.x-1, 'y':gear.y})
+    res.append({'x':gear.x-1, 'y':gear.y+1})
+    res.append({'x':gear.x, 'y':gear.y-1})
+    res.append({'x':gear.x, 'y':gear.y+1})
+    res.append({'x':gear.x+1, 'y':gear.y-1})
+    res.append({'x':gear.x+1, 'y':gear.y})
+    res.append({'x':gear.x+1, 'y':gear.y+1})
+    return res
 
-    found_symbol = False
-    # iterate over top row
-    curr_j = j1-1
-    curr_i = i1-1
-    while(curr_j<=j2+1):
-        if(number.val == 114):
-            print("searching for symbol at", curr_i, curr_j)
-        if (is_valid_position(curr_i, curr_j) and is_symbol(curr_i, curr_j)):
-            found_symbol = True
-            break
-        curr_j = curr_j + 1
-    
-    if found_symbol == False:
-        curr_j = j1-1
-        curr_i = i1+1
-        while(curr_j<=j2+1):
-            if(number.val == 114):
-                print("searching for symbol at", curr_i, curr_j)
-            if (is_valid_position(curr_i, curr_j) and is_symbol(curr_i, curr_j)):
-                found_symbol = True
-                break
-            curr_j = curr_j + 1
-    
-    if found_symbol == False:
-        found_symbol = is_valid_position(i1, j1-1) and is_symbol(i1, j1-1) 
-    
-    if found_symbol == False:
-        found_symbol = is_valid_position(i1, j2+1) and is_symbol(i1, j2+1)
-    print("found symbol result for:", number.val, found_symbol)
-    if found_symbol == True:
-        print("found symbol in vicintiy of ", number.val)
-        sum = sum + number.val
-    
+for gear in all_gears:
+    # print("gear is at", gear.x, gear.y)
+    adjacency_of_gear = get_adjacency_of_gear(gear)
+    # print(adjacency_of_gear)
+    adjacent_numbers = []
+    for pos in adjacency_of_gear:
+        if is_valid_position(pos["x"], pos["y"]):
+            for number in all_numbers:
+                if (is_number_adjacent_to(number, pos["x"], pos["y"])):
+                    # print("adjacent number found", number.val)
+                    adjacent_numbers.append(number)
+    product_of_numbers = 1
+    # print("set of adjacent number", set(adjacent_numbers))
+    gear_ratio_numbers = set(adjacent_numbers)
+    if len(gear_ratio_numbers) == 2:
+        for num in gear_ratio_numbers:
+            product_of_numbers = product_of_numbers * num.val
+        sum = sum + product_of_numbers
+
 print(sum)
-    
-    
-
-
